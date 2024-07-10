bc505d4639ea4d9edb972c3b4b271e9d8ebd2086
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-07-10 11:32:18 +0530

    introduced jwt for access & refresh token, and bcrypt for pwd encryption

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
 
 const userSchema=new Schema({
         username:{
@@ -53,4 +55,39 @@ const userSchema=new Schema({
     {timestamps:true}                                  //you will get 2 more fields: createdAt & updatedAt
 )
 
+//this is what we call as a pre hook in mongoose, like we can specify what to do if certain event is going to take place on the database.
+//for excample, whenever data will be saved, it will first encrypt the password and then save
+userSchema.pre("save", async function(next){
+
+    if(!this.modified("password")) return next()
+
+    this.password=bcrypt.hash(this.password, 10)
+    
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+//by using methods and followed by name we can create some methods too, 
+//remember the protype thing in js ? exactly the same, we are introducing more methods, 
+//so that we can directly use them wherever wanted
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign({
+        _id: this._id 
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+ )
+}
+
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+        _id: this._id 
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+ )
+}
 export const User=mongoose.model("User",userSchema)
